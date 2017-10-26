65cf357fa2e3b7d69e17e89c5c72367ec7438aa3
Przemek Formela <przem.formela@gmail.com>
2017-10-26 22:24:10 +0200

    Initial commit - Interface base
    Signed-off-by: Przemek Formela <przem.formela@gmail.com>

---
+++ app/src/main/java/yacs5e/ptpthingers/com/yacs5e_app/Character.java
@@ -0,0 +1,15 @@
+package yacs5e.ptpthingers.com.yacs5e_app;
+
+import android.media.Image;
+
+/**
+ * Created by leo on 26.10.17.
+ */
+
+class Character {
+
+    String name;
+    String details;
+    Image portrait;
+
+}
