639b3dd0f82ac99fe4229f2c19a6ad4f0b8d0671
Przemek Formela <przem.formela@gmail.com>
2017-12-17 16:39:24 +0100

    Added Registration to Login Screen

--- app/src/main/java/com/ptpthingers/yacs5e_app/LoginScreen.java
+++ app/src/main/java/com/ptpthingers/yacs5e_app/LoginScreen.java
@@ -2,11 +2,9 @@ package com.ptpthingers.yacs5e_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ptpthingers.grpctasks.GrpcLogin;
@@ -17,31 +15,35 @@ import java.util.concurrent.ExecutionException;
 
 public class LoginScreen extends AppCompatActivity {
 
-    private Button mSendButton;
+    private Button mLoginButton, mRegisterButton;
     private EditText mLoginText, mPassText;
-    private TextView mResultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        mSendButton = (Button) findViewById(R.id.login_button);
-        mLoginText = (EditText) findViewById(R.id.login_text);
-        mPassText = (EditText) findViewById(R.id.pass_text);
-        mResultText = (TextView) findViewById(R.id.result_text);
-        mResultText.setMovementMethod(new ScrollingMovementMethod());
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mRegisterButton = (Button) findViewById(R.id.register_button);
+        mLoginText = (EditText) findViewById(R.id.login_username);
+        mPassText = (EditText) findViewById(R.id.login_password);
 
-        mSendButton.setOnClickListener(mLoginListener);
+        mLoginButton.setOnClickListener(mLoginListener);
+        mRegisterButton.setOnClickListener(mRegisterListener);
+    }
+
+    private boolean validate() {
+        if(mLoginText.getText().toString().isEmpty() || mPassText.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "You need to fill in the fields!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
     private View.OnClickListener mLoginListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if(mLoginText.getText().toString().isEmpty() || mPassText.getText().toString().isEmpty()) {
-                Toast.makeText(getApplicationContext(), "You need to fill in the fields!", Toast.LENGTH_SHORT).show();
-                return;
-            }
+            if (!validate()) return;
 
             GrpcResult result;
             try {
@@ -55,6 +57,16 @@ public class LoginScreen extends AppCompatActivity {
                 e.printStackTrace();
             }
             Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private View.OnClickListener mRegisterListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (!validate()) return;
+
+            //TODO
+
         }
     };
 }
